08bb8e100dde8d2d45be1dbb4d92ce4e1c67496c
Kazi G. Kadar <kazi.swapan@gmail.com>
2025-12-31 23:33:11 +0100

    KSWEB-1: use Font Awesome icons for footer socials

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,13 @@
 import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import { SITE } from "@/config";
 
 interface Social {
   name: string;
   href: string;
   linkTitle: string;
-  icon: (_props: Props) => Element;
+  icon?: (_props: Props) => Element;
+  iconClass?: string;
 }
 
 export const SOCIALS: Social[] = [
@@ -16,19 +15,37 @@ export const SOCIALS: Social[] = [
     name: "GitHub",
     href: "https://github.com/kgkswapan",
     linkTitle: `${SITE.title} on GitHub`,
-    icon: IconGitHub,
+    iconClass: "fa-brands fa-github",
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/kgkswapan/",
     linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
+    iconClass: "fa-brands fa-linkedin",
+  },
+  {
+    name: "Google Scholar",
+    href: "https://scholar.google.com/citations?user=Zbj4vPUAAAAJ&hl=en",
+    linkTitle: `${SITE.title} on Google Scholar`,
+    iconClass: "fa-brands fa-google-scholar",
+  },
+  {
+    name: "ResearchGate",
+    href: "https://www.researchgate.net/profile/Kazi-Gulam-Kadar",
+    linkTitle: `${SITE.title} on ResearchGate`,
+    iconClass: "fa-brands fa-researchgate",
+  },
+  {
+    name: "ORCID",
+    href: "https://orcid.org/0009-0003-0893-5660",
+    linkTitle: `${SITE.title} on ORCID`,
+    iconClass: "fa-brands fa-orcid",
   },
   {
     name: "Contact",
     href: "/contact",
     linkTitle: `Open the contact page for ${SITE.title}`,
-    icon: IconMail,
+    iconClass: "fa-solid fa-envelope",
   },
 ] as const;
 
